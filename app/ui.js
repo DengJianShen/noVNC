ef9b58aec3d1c507729482358083f27e83849c09
dengjianshen <dengjianshen@yeah.net>
2022-11-18 15:46:05 +0800

    delete audio

--- app/ui.js
+++ app/ui.js
@@ -1887,21 +1887,22 @@ const UI = {
     },
 
     bell(e) {
-        if (WebUtil.getConfigVar('bell', 'on') === 'on') {
-            const promise = document.getElementById('noVNC_bell').play();
-            // The standards disagree on the return value here
-            if (promise) {
-                promise.catch((e) => {
-                    if (e.name === "NotAllowedError") {
-                        // Ignore when the browser doesn't let us play audio.
-                        // It is common that the browsers require audio to be
-                        // initiated from a user action.
-                    } else {
-                        Log.Error("Unable to play bell: " + e);
-                    }
-                });
-            }
-        }
+        // dengjianshen
+        // if (WebUtil.getConfigVar('bell', 'on') === 'on') {
+        //     const promise = document.getElementById('noVNC_bell').play();
+        //     // The standards disagree on the return value here
+        //     if (promise) {
+        //         promise.catch((e) => {
+        //             if (e.name === "NotAllowedError") {
+        //                 // Ignore when the browser doesn't let us play audio.
+        //                 // It is common that the browsers require audio to be
+        //                 // initiated from a user action.
+        //             } else {
+        //                 Log.Error("Unable to play bell: " + e);
+        //             }
+        //         });
+        //     }
+        // }
     },
 
     //Helper to add options to dropdown.
